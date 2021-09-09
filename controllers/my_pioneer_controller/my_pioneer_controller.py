d95c3e7efe7d0ad41a49089b2387b7b71636ad72
Claudia <claudiarecchia97@gmail.com>
2021-09-09 10:50:25 +0200

    box 1 portato in un angolo TODO: prossimo box con verifica del colore differente

--- controllers/my_pioneer_controller/my_pioneer_controller.py
+++ controllers/my_pioneer_controller/my_pioneer_controller.py
@@ -18,6 +18,13 @@ prev_sensors = []
 target_obj_initial_position = None
 left = False
 right = False
+before_turning_left_compass = []
+turn_left = False
+before_turning_left_gps = []
+straight = False
+box_placed = False
+placed_boxes = 0
+total_boxes = 2
 
 # Robot instance.
 robot = Robot()
@@ -47,6 +54,15 @@ d5 = robot.getDevice('so5')
 d6 = robot.getDevice('so6')
 d7 = robot.getDevice('so7')
 
+d8 = robot.getDevice('so8')
+d9 = robot.getDevice('so9')
+d10 = robot.getDevice('so10')
+d11 = robot.getDevice('so11')
+d12 = robot.getDevice('so12')
+d13 = robot.getDevice('so13')
+d14 = robot.getDevice('so14')
+d15 = robot.getDevice('so15')
+
 d0.enable(TIME_STEP)
 d1.enable(TIME_STEP)
 d2.enable(TIME_STEP)
@@ -56,13 +72,102 @@ d5.enable(TIME_STEP)
 d6.enable(TIME_STEP)
 d7.enable(TIME_STEP)
 
-sensors = [d0, d1, d2, d3, d4, d5, d6, d7]
+d8.enable(TIME_STEP)
+d9.enable(TIME_STEP)
+d10.enable(TIME_STEP)
+d11.enable(TIME_STEP)
+d12.enable(TIME_STEP)
+d13.enable(TIME_STEP)
+d14.enable(TIME_STEP)
+d15.enable(TIME_STEP)
+
+sensors = [d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11, d12, d13, d14, d15]
 # print("tipologia:", d0.getType())
 
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
-leftMotor.setVelocity(0.5 * MAX_SPEED)
-rightMotor.setVelocity(0.5 * MAX_SPEED)
+leftMotor.setVelocity(0.6 * MAX_SPEED)
+rightMotor.setVelocity(0.6 * MAX_SPEED)
+
+
+def check_wall_reached():
+    global count, wall_reached_gps, wall_reached_compass, wall_reached
+    # più di 1 sensore misura un valore elevato => parete
+    if sum(s.getValue() > 950 for s in sensors) > 1:
+        print("WALL REACHED")
+        wall_reached_gps = gps_sensor.getValues()
+        wall_reached_compass = compass.getValues()
+        # tornare indietro per riprendere il box (in base al sensore gps)
+        # if all(s.getValue() > 700 for s in high_sensors):
+        print("GO BACK")
+        leftMotor.setVelocity(-0.6 * MAX_SPEED)
+        rightMotor.setVelocity(-0.6 * MAX_SPEED)
+        wall_reached = True
+        return wall_reached
+    else:
+        return False
+
+
+def turn_after_wall_reached():
+    global stopped, wall_reached_gps, wall_reached, before_turning_left_compass, turn_left, before_turning_left_gps
+    print("QUIIIII")
+    # print(abs(wall_reached_gps[0] - gps_sensor.getValues()[0]))
+    # print(abs(wall_reached_gps[1] - gps_sensor.getValues()[1]))
+    # print(abs(wall_reached_gps[2] - gps_sensor.getValues()[2]))
+    if abs(wall_reached_gps[0] - gps_sensor.getValues()[0]) > 0.3 or abs(
+            wall_reached_gps[1] - gps_sensor.getValues()[1]) > 0.3 or abs(
+        wall_reached_gps[2] - gps_sensor.getValues()[2]) > 0.3:
+        print("STOP")
+        leftMotor.setVelocity(0)
+        rightMotor.setVelocity(0)
+        stopped = True
+    if stopped:
+        print("TURN RIGHT")
+        leftMotor.setVelocity(0.2 * MAX_SPEED)
+        rightMotor.setVelocity(0)
+        # print(abs(round(wall_reached_compass[0]) - round(compass.getValues()[2])))
+        # print(abs((wall_reached_compass[0]) - (compass.getValues()[2])))
+        if round(abs(wall_reached_compass[0]) - (compass.getValues()[2]), 3) == 1.999:
+            print("saved")
+            before_turning_left_compass = compass.getValues()
+            before_turning_left_gps = gps_sensor.getValues()
+            turn_left = True
+            wall_reached = False
+            stopped = False
+            leftMotor.setVelocity(0.6 * MAX_SPEED)
+            rightMotor.setVelocity(0.6 * MAX_SPEED)
+
+
+def do_turn_left(before_turning_left_compass, before_turning_left_gps):
+    global turn_left, straight
+    print("valori")
+    print(abs(before_turning_left_gps[0] - gps_sensor.getValues()[0]))
+    print(abs(before_turning_left_gps[1] - gps_sensor.getValues()[1]))
+    print(abs(before_turning_left_gps[2] - gps_sensor.getValues()[2]))
+    if abs(before_turning_left_gps[0] - gps_sensor.getValues()[0]) > 0.5 or \
+            abs(before_turning_left_gps[1] - gps_sensor.getValues()[1]) > 0.5 or \
+            abs(before_turning_left_gps[2] - gps_sensor.getValues()[2]) > 0.5:
+        print("TURN LEFT")
+        leftMotor.setVelocity(0)
+        rightMotor.setVelocity(0.2 * MAX_SPEED)
+        print(abs(round(before_turning_left_compass[0]) - round(compass.getValues()[2])))
+        print(abs((before_turning_left_compass[0]) - (compass.getValues()[2])))
+        if round(abs(before_turning_left_compass[0] - (compass.getValues()[2])), 2) == 0.97 and d7.getValue() > 900:
+            print("vai avanti")
+            turn_left = False
+            straight = True
+
+
+def go_straight():
+    print("avanti")
+    leftMotor.setVelocity(0.6 * MAX_SPEED)
+    rightMotor.setVelocity(0.6 * MAX_SPEED)
+
+def check_for_other_box():
+    print("check for box")
+    leftMotor.setVelocity(-0.6 * MAX_SPEED)
+    rightMotor.setVelocity(-0.6 * MAX_SPEED)
+
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -84,120 +189,59 @@ while robot.step(TIME_STEP) != -1:
         target_object = camera.getRecognitionObjects()[0]
         target_obj_initial_position = camera.getRecognitionObjects()[0].get_position()
 
-
-
     print("target obj:", target_object)
     print("compass:", compass.getValues())
 
-    for sensor in sensors:
-        # se ho un target obj devo verificare che mi ci sto avvicinando
-        # object_position deve avere valori decrescenti (e fermarmi a circa [X, X, 0.2] quando tocco l'oggetto)
-        # devo minimizzare anche il primo parametro per portare il cubo al centro del robot
 
-        # if target_object is not None:
-        #     if camera.getRecognitionObjects()[0].get_position()[0] > target_obj_initial_position[0] or camera.getRecognitionObjects()[0].get_position()[1] > target_obj_initial_position[1] or camera.getRecognitionObjects()[0].get_position()[2] > target_obj_initial_position[2]:
-        #         print("mi sto allontanando dall'oggetto target")
-
-        if target_object is not None:
-            if left is True:
-                left = False
-                print("DRITTO")
-                leftMotor.setVelocity(0.5 * MAX_SPEED)
-                rightMotor.setVelocity(0.5 * MAX_SPEED)
-            if right is True:
-                right = False
-                print("DRITTO")
-                leftMotor.setVelocity(0.5 * MAX_SPEED)
-                rightMotor.setVelocity(0.5 * MAX_SPEED)
-            if camera.getRecognitionObjects()[0].get_position()[0] < 0:  # il target è a sinistra
-                # giro leggermente a sinistra per centrarlo
-                print("TARGET A SINISTRA")
-                left = True
-                leftMotor.setVelocity(0)
-                rightMotor.setVelocity(0.5 * MAX_SPEED)
-            if camera.getRecognitionObjects()[0].get_position()[0] > 0:  # il target è a destra
-                # giro leggermente a destra per centrarlo
-                print("TARGET A DESTRA")
-                right = True
-                leftMotor.setVelocity(0.5 * MAX_SPEED)
-                rightMotor.setVelocity(0)
-
-        if sensor.getValue() > 900:  # wall reached
-            count += 1
-            high_sensors.append(sensor)
-        if count > 1:
-            count = 0
-            print("WALL REACHED")
-            wall_reached_gps = gps_sensor.getValues()
-            wall_reached_compass = compass.getValues()
-            wall_reached = True
-            # tornare indietro per riprendere il box (in base al sensore gps)
-            # if all(s.getValue() > 700 for s in high_sensors):
-            print("GO BACK")
-            leftMotor.setVelocity(-0.5 * MAX_SPEED)
-            rightMotor.setVelocity(-0.5 * MAX_SPEED)
-            high_sensors = []
-
-    if wall_reached:
-        # print(abs(wall_reached_gps[0] - gps_sensor.getValues()[0]))
-        # print(abs(wall_reached_gps[1] - gps_sensor.getValues()[1]))
-        # print(abs(wall_reached_gps[2] - gps_sensor.getValues()[2]))
-        if abs(wall_reached_gps[0] - gps_sensor.getValues()[0]) > 0.3 or abs(wall_reached_gps[1] - gps_sensor.getValues()[1]) > 0.3 or abs(wall_reached_gps[2] - gps_sensor.getValues()[2]) > 0.3:
-            print("STOP")
+    if target_object is not None and not wall_reached and camera.getRecognitionNumberOfObjects() > 0:
+        if left:
+            left = False
+            print("DRITTO")
+            go_straight()
+        if right:
+            right = False
+            print("DRITTO")
+            go_straight()
+        if camera.getRecognitionObjects()[0].get_position()[0] < -0.01:  # il target è a sinistra
+            # giro leggermente a sinistra per centrarlo
+            print("TARGET A SINISTRA")
+            left = True
             leftMotor.setVelocity(0)
+            rightMotor.setVelocity(0.6 * MAX_SPEED)
+        if camera.getRecognitionObjects()[0].get_position()[0] > 0.01:  # il target è a destra
+            # giro leggermente a destra per centrarlo
+            print("TARGET A DESTRA")
+            right = True
+            leftMotor.setVelocity(0.6 * MAX_SPEED)
             rightMotor.setVelocity(0)
-            stopped = True
-        if stopped:
-            print("TURN RIGHT")
-            leftMotor.setVelocity(0.2 * MAX_SPEED)
-            rightMotor.setVelocity(0)
-            print(abs(round(wall_reached_compass[0]) - round(compass.getValues()[2])))
-            print(abs((wall_reached_compass[0]) - (compass.getValues()[2])))
-            if round(abs(wall_reached_compass[0]) - (compass.getValues()[2]), 3) == 1.999:
 
-                wall_reached = False
-                stopped = False
-                leftMotor.setVelocity(0.5 * MAX_SPEED)
-                rightMotor.setVelocity(0.5 * MAX_SPEED)
+    check_wall_reached()
+    if wall_reached: turn_after_wall_reached()
+    if turn_left: do_turn_left(before_turning_left_compass, before_turning_left_gps)
+    if straight: go_straight()
+    if camera.getRecognitionNumberOfObjects() == 1 and wall_reached:
+        objects = camera.getRecognitionObjects()
+        obj = objects[0]
+        if round(obj.get_position()[0], ) == 0 and \
+                round(obj.get_position()[1], 0) == 0 and \
+                round(obj.get_position()[2], 0) == 0 and \
+                round(gps_sensor.getValues()[0], 1) == -3.1 and round(gps_sensor.getValues()[1], 1) == -0.2 and \
+                round(gps_sensor.getValues()[2], 1) == -3.5:
+            straight = False
+            box_placed = True
+            placed_boxes += 1
+            if placed_boxes < total_boxes:
+                check_for_other_box()
 
-    print("target obj position", camera.getRecognitionObjects()[0].get_position())
     print("target obj pos on image", target_object.get_position_on_image())
     print("gps sensor", gps_sensor.getValues())
-
-    # for sensor in sensors:
-    #     if sensor.getValue() > 100:
-    #         count += 1
-    #     if count > 4:  # wall reached
-    #         print("STOP")
-    #         if prev_compass is None:
-    #             prev_compass = compass.getValues()
-    #
-    #         if prev_compass[2] - compass.getValues()[2] < 0.99:
-    #             leftMotor.setVelocity(0.5 * MAX_SPEED)
-    #             rightMotor.setVelocity(0)
-    #         else:
-    #             prev_compass = None
-    #             leftMotor.setVelocity(0.5 * MAX_SPEED)
-    #             rightMotor.setVelocity(0.5 * MAX_SPEED)
-
-    # if d0.getValue() > 1000:
-    #     # if d12.getValue() < d3.getValue():
-    #     #     leftMotor.setVelocity(0.5 * MAX_SPEED)
-    #     #     rightMotor.setVelocity(0)
-    #     # else:
-    #     #     leftMotor.setVelocity(0)
-    #     #     rightMotor.setVelocity(0.5 * MAX_SPEED)
-    #     print("STOP")
-    #     stop = True
-    #     leftMotor.setVelocity(0)
-    #     rightMotor.setVelocity(0)
-
 
     print("n_objects = ", camera.getRecognitionNumberOfObjects())
     objects = camera.getRecognitionObjects()
     print("objects = ", objects)
     for obj in objects:
-        print(obj.get_colors())
+        print("colore:", obj.get_colors())
+        print("posizione:", obj.get_position())
     print("\n")
 
     pass
